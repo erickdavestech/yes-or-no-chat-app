dd17bae0a53ae2ade01a616a6fd7ca64eb7eb3ec
x <erickdavestech@gmail.com>
2024-08-02 00:51:51 -0400

    _00_theme_and_home-screen

---
+++ lib/Pages/chat_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(Object context) {
+    return const Placeholder();
+  }
+}

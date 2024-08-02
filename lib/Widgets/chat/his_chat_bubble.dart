2e065e71e350a0174a15abd34a4c9b418423f123
x <erickdavestech@gmail.com>
2024-08-02 01:33:01 -0400

    yes_or_no_chat_screen

---
+++ lib/Widgets/chat/his_chat_bubble.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class HisMessageBubble extends StatelessWidget {
+  const HisMessageBubble({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

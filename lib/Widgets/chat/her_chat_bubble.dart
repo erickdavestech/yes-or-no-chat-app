bfbc6ebc35819ad8a6a66668e455887ee718b5bf
x <erickdavestech@gmail.com>
2024-08-02 13:03:46 -0400

    yes_or_no_chat_her_chat_screen

--- lib/Widgets/chat/her_chat_bubble.dart
+++ lib/Widgets/chat/her_chat_bubble.dart
@@ -1,10 +1,61 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class HerMessageBubble extends StatelessWidget {
   const HerMessageBubble({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // los colors Scheme del context de este widget
+    final colors = Theme.of(context).colorScheme;
+    // Para verlos en debug console
+    // print('estos son los colorScheme: ..$colors');
+
+    return Column(
+      // Alinimaos el contenido de la columna a la izquierda o al pricipio
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+              color: colors.secondary, borderRadius: BorderRadius.circular(20)),
+          child: const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: Text(
+              'Hola Mundo',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ),
+        const SizedBox(height: 5),
+        _ImageBubble(),
+        const SizedBox(height: 10),
+      ],
+    );
+  }
+}
+
+//
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Image.network(
+          'https://yesno.wtf/assets/no/20-56c4b19517aa69c8f7081939198341a4.gif',
+          width: size.width * 0.7,
+          height: 150,
+          fit: BoxFit.cover,
+          loadingBuilder: (context, child, loadingProgress) {
+            if (loadingProgress == null) return child;
+
+            return Container(
+              width: size.width * 0.7,
+              height: 150,
+              padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+              child: const Text('Mi amor está enviando una imagen'),
+            );
+          },
+        ));
   }
 }
